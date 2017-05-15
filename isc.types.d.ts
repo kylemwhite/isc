685d57a8b32259c6f74b82ed50f12661f416f6d7
kwhite <kwhite@ETSSMF00W932>
2017-05-15 16:40:37 -0700

    Automatic SNAPSHOT_v11.1d_2017-05-14

--- isc.types.d.ts
+++ isc.types.d.ts
@@ -3,7 +3,7 @@
 // Project: https://gihub.com/kylemwhite/isc
 // Definitions by: Kyle White <http://kmwTech.com>
 // Definitions: https://gihub.com/DefinitelyTyped/DefinitelyTyped
-// Generated: 5/15/2017 4:33:17 PM by kwhite
+// Generated: 5/15/2017 4:40:11 PM by KWhite
 // Generated from SmartClient version SNAPSHOT_v11.1d_2017-05-14
 
 declare namespace Isc {
