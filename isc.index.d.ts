e8a067588f4f804f7cd5cfbb2b83d50ade11d87e
kwhite <kwhite@ETSSMF00W932>
2017-05-22 15:36:09 -0700

    Automatic SNAPSHOT_v11.1d_2017-05-21

--- isc.index.d.ts
+++ isc.index.d.ts
@@ -7,7 +7,7 @@
 // Project: https://gihub.com/kylemwhite/isc
 // Definitions by: Kyle White <http://kmwTech.com>
 // Definitions: https://gihub.com/kylemwhite/isc
-// Generated: 5/22/2017 3:30:08 PM by kwhite
+// Generated: 5/22/2017 3:36:05 PM by kwhite
 // Generated from SmartClient version SNAPSHOT_v11.1d_2017-05-21
 
 /**
