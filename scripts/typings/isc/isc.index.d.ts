8f1b9f0928a437857f0d5aa1a3eb431d24111ce0
kwhite <kwhite@ETSSMF00W932>
2017-06-17 07:01:18 -0700

    Automatic versionNumber=SNAPSHOT_v11.1d_2017-06-16

--- scripts/typings/isc/isc.index.d.ts
+++ scripts/typings/isc/isc.index.d.ts
@@ -6,7 +6,7 @@
 // Project: https://gihub.com/kylemwhite/isc
 // Definitions by: Kyle White <http://kmwTech.com>
 // Definitions: https://gihub.com/kylemwhite/isc
-// Generated: 6/16/2017 10:52:00 AM by KWhite
+// Generated: 6/17/2017 7:01:10 AM by KWhite
 // Generated from SmartClient version SNAPSHOT_v11.1d_2017-06-16
 
 /**
--- scripts/typings/isc/isc.interfaces.d.ts
+++ scripts/typings/isc/isc.interfaces.d.ts
@@ -4,7 +4,7 @@
 // Project: https://gihub.com/kylemwhite/isc
 // Definitions by: Kyle White <http://kmwTech.com>
 // Definitions: https://gihub.com/DefinitelyTyped/DefinitelyTyped
-// Generated: 6/16/2017 10:51:03 AM by KWhite
+// Generated: 6/17/2017 7:00:13 AM by KWhite
 // Generated from SmartClient version SNAPSHOT_v11.1d_2017-06-16
 
 declare namespace Isc {
