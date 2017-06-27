50ba70dec0915ac77f53ba6dd3c6fd0e6b48b1e6
kwhite <kwhite@ETSSMF00W932>
2017-06-27 08:10:21 -0700

    Automatic versionNumber=SNAPSHOT_v11.1d_2017-06-25

--- Progress.md
+++ Progress.md
@@ -4,7 +4,7 @@ By Kyle White ( [KMW Tech Solutions, Inc.](http://kmwTech.com/) )
 
 ## Progress Report
  
-Generated from SmartClient SNAPSHOT_v11.1d_2017-06-25 at 6/26/2017 7:01:15 AM by KWhite
+Generated from SmartClient SNAPSHOT_v11.1d_2017-06-25 at 6/27/2017 8:10:11 AM by KWhite
 
 To see the details, look in the [Errors.txt](./Errors.txt) file.
 
--- scripts/typings/isc/isc.interfaces.d.ts
+++ scripts/typings/isc/isc.interfaces.d.ts
@@ -4,7 +4,7 @@
 // Project: https://gihub.com/kylemwhite/isc
 // Definitions by: Kyle White <http://kmwTech.com>
 // Definitions: https://gihub.com/DefinitelyTyped/DefinitelyTyped
-// Generated: 6/26/2017 7:00:10 AM by KWhite
+// Generated: 6/27/2017 8:09:05 AM by KWhite
 // Generated from SmartClient version SNAPSHOT_v11.1d_2017-06-25
 
 declare namespace Isc {
