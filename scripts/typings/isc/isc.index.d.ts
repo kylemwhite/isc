723bf105b304dea510fdc220520004b9861c2fd8
kwhite <kwhite@ETSSMF00W932>
2017-11-04 07:01:42 -0700

    Automatic versionNumber=SNAPSHOT_v12.0d_2017-11-04

--- scripts/typings/isc/isc.index.d.ts
+++ scripts/typings/isc/isc.index.d.ts
@@ -6,8 +6,8 @@
 // Project: https://gihub.com/kylemwhite/isc
 // Definitions by: Kyle White <http://kmwTech.com>
 // Definitions: https://gihub.com/kylemwhite/isc
-// Generated: 11/3/2017 7:01:39 AM by KWhite
-// Generated from SmartClient version SNAPSHOT_v12.0d_2017-11-03
+// Generated: 11/4/2017 7:01:30 AM by KWhite
+// Generated from SmartClient version SNAPSHOT_v12.0d_2017-11-04
 
 /**
  * Generic extensions to JavaScript Arrays. You can call these on any Array.
--- scripts/typings/isc/isc.objects.d.ts
+++ scripts/typings/isc/isc.objects.d.ts
@@ -6,8 +6,8 @@
 // Project: https://gihub.com/kylemwhite/isc
 // Definitions by: Kyle White <http://kmwTech.com>
 // Definitions: https://gihub.com/DefinitelyTyped/DefinitelyTyped
-// Generated: 11/3/2017 7:00:11 AM by KWhite
-// Generated from SmartClient version SNAPSHOT_v12.0d_2017-11-03
+// Generated: 11/4/2017 7:00:03 AM by KWhite
+// Generated from SmartClient version SNAPSHOT_v12.0d_2017-11-04
 
 declare namespace Isc {
 
