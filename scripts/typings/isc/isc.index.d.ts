0022a4150820014df78a103d370eb11f4c052097
kwhite <kwhite@ETSSMF00W932>
2017-12-05 07:01:41 -0800

    Automatic versionNumber=SNAPSHOT_v12.0d_2017-12-02

--- scripts/typings/isc/isc.index.d.ts
+++ scripts/typings/isc/isc.index.d.ts
@@ -6,7 +6,7 @@
 // Project: https://gihub.com/kylemwhite/isc
 // Definitions by: Kyle White <http://kmwTech.com>
 // Definitions: https://gihub.com/kylemwhite/isc
-// Generated: 12/4/2017 7:01:35 AM by KWhite
+// Generated: 12/5/2017 7:01:29 AM by KWhite
 // Generated from SmartClient version SNAPSHOT_v12.0d_2017-12-02
 
 /**
